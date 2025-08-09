6b28055f7ff78294549f69b4332d1867e63970d8
Gandeevi <gandeevi@gmail.com>
2025-08-09 19:50:29 +0530

    loci

--- src/components/Location.js
+++ src/components/Location.js
@@ -95,10 +95,11 @@ const ImageLabeler = ({ initialImage = null, initialPins = [], onDataChange = ()
 
   return (
     <div className="container mt-4">
-      <h3>Image Pin Drop-1</h3>
+    
+    
 
       <Form.Group controlId="formFile" className="mb-3">
-        <Form.Label>Upload Image</Form.Label>
+        <Form.Label>Add a Loci Image</Form.Label>
         <Form.Control type="file" onChange={handleImageUpload} />
       </Form.Group>
 
